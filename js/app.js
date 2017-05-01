c4f5262a6b9ebfca3fe805ab9ce1bc4c8f28354b
Jeff Berlin <jeff@jbrally.com>
2017-05-01 14:50:27 -0400

    Google Maps Geolocation is working
    Google Maps Geolocation is working

--- js/app.js
+++ js/app.js
@@ -264,6 +264,7 @@ var bounds;
 function initMap() {
 
 	bounds = new google.maps.LatLngBounds();
+
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {
 			lat: 34.2257,
@@ -271,9 +272,33 @@ function initMap() {
 		},
 		zoom: 11
 	});
+	infoWindow = new google.maps.InfoWindow;
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			infoWindow.setPosition(pos);
+			infoWindow.setContent("Location Found.");
+			infoWindow.open(map);
+			map.setCenter(pos);
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		})
+	} else {
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+		infoWindow.setContent(browserHasGeolocation ? 
+			'Error: Please enable geolocation.' :
+			'Error: Geolocation failed.');
+
+		infoWindow.open(map);
+	}
 }
 window.initMap = initMap;
-
-
-
-
